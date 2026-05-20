86559a6ede8eeaae36f4e786ce8442c7767f06a0
Sarfaraj Ansari <imsarfaraaj7@gmail.com>
2026-05-20 20:03:07 +0530

    sugar mill initial

---
+++ src/main/java/com/mill/sugarmill/SugarmillApplication.java
@@ -0,0 +1,13 @@
+package com.mill.sugarmill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugarmillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SugarmillApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mill/sugarmill/SugarmillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mill.sugarmill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SugarmillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
